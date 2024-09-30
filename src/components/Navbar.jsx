864a0387ade624822ebe8470eabd1bed4d559b3e
Larry <jonrickcarlocacanando@gmail.com>
2024-09-30 19:31:45 +0800

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Icon from '../assets/Icon.png';
 import {Link} from "react-scroll";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isSticky, setIsSticky] = useState(false);
+
+        const toggleMenu = () => {
+            setIsMenuOpen(!isMenuOpen);
+        }
 
     useEffect(() => {
         const handleScroll = () => {
@@ -34,15 +40,35 @@ const Navbar = () => {
 
  return (
     <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-        <nav>
-           <div>
-            <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={Icon} alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]'>NEXCENT</span></a>
-           <ul className='md:flex space-x-12 hidden'>
+        <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 border-b bg-white duration-300" : ""}`}>
+            <div className='flex justify-between items-center text-base gap-8'>
+                <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={Icon} alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]'>NEXCENT</span></a>
+            
+                <ul className='md:flex space-x-12 hidden'>
                 {
-                    navItems.map(({link, path}) =><Link to={path}>{link}</Link>)
+                    navItems.map(({link, path}) =><Link to={path} spy={true} smooth={true} offset={-100} key={path} className='block text-base text-gray900 hover:text-brandPrimary first:font-medium'>{link}</Link>)
                 } 
 
-           </ul>
+                </ul>
+                <div className='space-x-12 hidden lg:flex items-center'>        
+                    <a href="/" className='hidden lg:flex items-center text-brandPrimary hover:text-gray900'>Login</a>
+                    <button className='bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGrey'>Sign up</button>
+                </div>
+                <div className='md:hidden'>
+                    <button 
+                    onClick={toggleMenu}
+                    className=' text-neutralDGrey focus:outline-none focus:text-green-500'>
+                        {
+                         isMenuOpen ? (<FaXTwitter className='h-6 w-6'/>) : (<FaBarsStaggered className='h-6 w-6 '/>)
+                        }
+                </button>
+                </div>
+            </div>
+           
+           <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${ isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+            {
+                navItems.map(({link, path}) => <Link to={path} spy={true} smooth={true} offset={-100} key={path} className='block text-base text-white hover:text-brandPrimary first:font-medium'>{link}</Link>)   
+            }
            </div>
         </nav>
     </header>
